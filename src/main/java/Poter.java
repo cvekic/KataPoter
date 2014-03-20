ede4b81855a602ac07824fb24544679433dbd4ad
Mladjan Cvijanovic <mladjan.cvijanovic@hotmail.com>
2014-03-20 21:22:57 +0100

    added class Poter and PoterTest

---
+++ src/main/java/Poter.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Mladjan on 20.3.2014.
+ */
+public class Poter {
+
+    
+
+}
---
+++ src/test/java/PoterTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+
+/**
+ * Created by Mladjan on 20.3.2014.
+ */
+public class PoterTest {
+
+    Poter subject;
+
+    @Before
+    public void setUp() {
+        subject = new Poter();
+    }
+
+
+
+}
